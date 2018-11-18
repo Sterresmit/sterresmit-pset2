6935b3f4ad34f31a120976d274f25cc9b7269264
Sterre <sterresmit98@gmail.com>
2018-11-18 22:56:17 +0100

    Initial Commit

---
+++ sterresmit-pset2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sterresmit-pset2
+//
+//  Created by Sterre Smit on 18/11/2018.
+//  Copyright © 2018 Sterre Smit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ sterresmit-pset2Tests/sterresmit_pset2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  sterresmit_pset2Tests.swift
+//  sterresmit-pset2Tests
+//
+//  Created by Sterre Smit on 18/11/2018.
+//  Copyright © 2018 Sterre Smit. All rights reserved.
+//
+
+import XCTest
+@testable import sterresmit_pset2
+
+class sterresmit_pset2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
